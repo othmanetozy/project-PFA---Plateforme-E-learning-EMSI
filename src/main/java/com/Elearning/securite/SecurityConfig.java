54a7b36f7277d72447947553435c4139be49db80
othmanetozy <tozyothmane123@gmail.com>
2024-01-07 22:52:50 +0100

    Adding Resources

--- src/main/java/com/Elearning/securite/SecurityConfig.java
+++ src/main/java/com/Elearning/securite/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.Elearning.securite;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,23 +13,21 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
-public class SecurityConfig {
-
-
+public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
                 .disable()
-                .authorizeHttpRequests()
-                .requestMatchers( "Login")
-                .permitAll()
+                .authorizeRequests()
+                .requestMatchers("/h2-console/**", "/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         return http.build();
     }
+
+
 }
