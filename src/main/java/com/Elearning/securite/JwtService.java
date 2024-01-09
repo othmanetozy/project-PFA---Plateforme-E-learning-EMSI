5e3321d46a8f79ed702f2da16abd63ee69bacb53
othmanetozy <tozyothmane123@gmail.com>
2024-01-09 18:29:40 +0100

    Extract the username from the token

--- src/main/java/com/Elearning/securite/JwtService.java
+++ src/main/java/com/Elearning/securite/JwtService.java
@@ -2,15 +2,28 @@ package com.Elearning.securite;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "f8834a80c4b91970a9faa79b69549dbe9a3711f7c11f8fb71c65f91a84f0ef2f";
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaims(token,Claims::getSubject);
     }
+
+    public <T> T extractClaims(String token , Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts
                 .parserBuilder()
@@ -18,5 +31,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
