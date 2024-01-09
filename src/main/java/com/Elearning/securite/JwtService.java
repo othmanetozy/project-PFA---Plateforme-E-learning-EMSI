5493e1b991deab35736169387861af516e125cb2
othmanetozy <tozyothmane123@gmail.com>
2024-01-09 18:55:44 +0100

    Generate Token JWT

--- src/main/java/com/Elearning/securite/JwtService.java
+++ src/main/java/com/Elearning/securite/JwtService.java
@@ -2,12 +2,14 @@ package com.Elearning.securite;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Base64;
+import java.util.*;
 import java.util.function.Function;
 
 @Service
@@ -22,6 +24,26 @@ public class JwtService {
     public <T> T extractClaims(String token , Function<Claims,T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generatToken(new HashMap<>(), userDetails);
+    }
+
+
+    public String generatToken(
+            Map<String, Object>extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .compact();
+
     }
 
     private Claims extractAllClaims(String token){
