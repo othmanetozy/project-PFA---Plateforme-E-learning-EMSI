42cc2e892c7d5552698e3dae951428cb97a36af8
othmanetozy <tozyothmane123@gmail.com>
2024-01-09 14:30:41 +0100

    Add Key Secrets

--- src/main/java/com/Elearning/securite/JwtService.java
+++ src/main/java/com/Elearning/securite/JwtService.java
@@ -1,11 +1,22 @@
 package com.Elearning.securite;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
+    private static final String SECRET_KEY = "f8834a80c4b91970a9faa79b69549dbe9a3711f7c11f8fb71c65f91a84f0ef2f";
     public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
